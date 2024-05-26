d75f362fe0c6a7ba1a680d3c17e591a42946cac7
sen-sourabh <sourabhsen201313@gmail.com>
2024-05-26 16:50:41 +0530

    users migrations

--- src/database/migrations/1716714313286-create-roles.ts
+++ src/database/migrations/1716714313286-create-roles.ts
@@ -8,7 +8,7 @@ export class CreateRoles1716714313286 implements MigrationInterface {
     try {
       await queryRunner.query(`
         CREATE TABLE roles (
-            id varchar(150) NOT NULL DEFAULT (uuid()),
+            id int NOT NULL AUTO_INCREMENT,
             name varchar(255) character set utf8mb4 collate utf8mb4_0900_ai_ci NOT NULL,
             is_default tinyint NOT NULL DEFAULT '1',
             is_enabled tinyint NOT NULL DEFAULT '1',
---
+++ src/database/migrations/1716722093652-insert-users.ts
@@ -0,0 +1,19 @@
+import { Logger } from '@nestjs/common';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InsertUsers1716722093652 implements MigrationInterface {
+  private readonly logger = new Logger(InsertUsers1716722093652.name);
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    try {
+      await queryRunner.query(
+        `INSERT INTO users (first_name, organization_name, organization_position, no_of_employees, email, password, secret_key, roles_id, user_types_id) VALUES ('admin', 'Sample Org.', 'HR', 100, 'admin@yopmail.com', 'Welcome@123', 'uey!rs@2id=', 1, 1)`,
+      );
+      this.logger.log(`Up: Insert users executed`);
+    } catch (error) {
+      this.logger.error(`Up: Insert users have an error: `, error?.message);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
