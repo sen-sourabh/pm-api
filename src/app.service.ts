7ecd04cfef879144b485422ed327b4b8e840a2bb
sen-sourabh <sourabhsen201313@gmail.com>
2024-06-09 16:45:24 +0530

    some fixes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ApiResponseModel } from './core/shared/interfaces/api-response.interface';
 
 @Injectable()
 export class AppService {
   constructor(private readonly configService: ConfigService) {}
 
-  isServerRunning(): string {
-    return `Vault is Running on ${parseInt(this.configService.getOrThrow('port'))}`;
+  isServerRunning(): ApiResponseModel<string> {
+    return {
+      message: `Vault is Running on ${parseInt(this.configService.getOrThrow('port'))}`,
+    };
   }
 }
