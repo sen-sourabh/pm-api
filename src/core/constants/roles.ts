11f18a4b7ceaaf15d8b37340a9af6d2f513b1892
sen-sourabh <sourabhsen201313@gmail.com>
2024-06-10 15:31:50 +0530

    ROLES_Usertypes constants

---
+++ src/core/constants/roles.ts
@@ -0,0 +1,11 @@
+export const ROLES = [
+    {
+        name: 'Super Admin'
+    },
+    {
+        name: 'Admin'
+    },
+    {
+        name: 'Collaborator'
+    }
+];
---
+++ src/core/constants/usertypes.ts
@@ -0,0 +1,8 @@
+export const USERTYPES = [
+    {
+        name: 'Organization'
+    },
+    {
+        name: 'Individual'
+    }
+]
