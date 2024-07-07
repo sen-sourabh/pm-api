6881c9aa2cd2e2642df6c2ae33bfdad18f658c67
sen-sourabh <sourabhsen201313@gmail.com>
2024-07-07 15:46:35 +0530

    activity_logs migration create

---
+++ src/database/migrations/1720346348176-create-activity_logs.ts
@@ -0,0 +1,100 @@
+import { Logger } from '@nestjs/common';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateActivityLogs1720346348176 implements MigrationInterface {
+  private readonly logger = new Logger(CreateActivityLogs1720346348176.name);
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    try {
+      await queryRunner.createTable(
+        new Table({
+          name: 'activity_logs',
+          columns: [
+            {
+              name: 'id',
+              type: 'varchar',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'uuid',
+              isNullable: false,
+            },
+            {
+              name: 'handler',
+              type: 'varchar',
+              isNullable: true,
+              default: null,
+            },
+            {
+              name: 'method',
+              type: 'varchar',
+              isNullable: true,
+              default: null,
+            },
+            {
+              name: 'responseCode',
+              type: 'int',
+              isNullable: true,
+              default: null,
+            },
+            {
+              name: 'headers',
+              type: 'json',
+              isNullable: true,
+              default: null,
+            },
+            {
+              name: 'request',
+              type: 'json',
+              isNullable: true,
+              default: null,
+            },
+            {
+              name: 'response',
+              type: 'json',
+              isNullable: true,
+              default: null,
+            },
+            {
+              name: 'ipAddress',
+              type: 'varchar',
+              isNullable: true,
+              default: null,
+            },
+            {
+              name: 'location',
+              type: 'varchar',
+              isNullable: true,
+              default: null,
+            },
+            {
+              name: 'createdAt',
+              type: 'datetime',
+              default: 'CURRENT_TIMESTAMP',
+              isNullable: false,
+            },
+            {
+              name: 'updatedAt',
+              type: 'datetime',
+              default: 'CURRENT_TIMESTAMP',
+              onUpdate: 'CURRENT_TIMESTAMP',
+              isNullable: false,
+            },
+          ],
+        }),
+        false,
+      );
+      this.logger.log(`Up: Create activity logs executed`);
+    } catch (error) {
+      this.logger.error(`Up: Create activity logs has an error: `, error?.message);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    try {
+      await queryRunner.dropTable('activity_logs');
+      this.logger.log(`Down: Drop activity logs executed`);
+    } catch (error) {
+      this.logger.error(`Down: Drop activity logs has an error: `, error?.message);
+    }
+  }
+}
