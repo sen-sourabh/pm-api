da9c5d6a002d02038744554b7f0a27afd3f9b4be
sen-sourabh <sourabhsen201313@gmail.com>
2024-08-15 12:40:21 +0530

    removed dynamimc field modeule from core modules

--- src/core/modules/dynamic-fields/dynamic-fields.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DynamicFieldsService } from './dynamic-fields.service';
-
-@Module({
-  providers: [DynamicFieldsService],
-  exports: [DynamicFieldsService],
-})
-export class DynamicFieldsModule {}
