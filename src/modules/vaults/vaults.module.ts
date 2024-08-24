9b024c45f6ef7d8be0fbbe10d5e08409ecf42520
sen-sourabh <sourabhsen201313@gmail.com>
2024-08-24 15:07:38 +0530

    Webhook integration with Vaults module

--- src/modules/vaults/vaults.module.ts
+++ src/modules/vaults/vaults.module.ts
@@ -1,12 +1,13 @@
 import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActivityLogsModule } from '../../core/modules/activity-logs/activity-logs.module';
+import { WebhooksModule } from '../../core/modules/webhooks/webhooks.module';
 import { Vault } from './entities/vault.entity';
 import { VaultsController } from './vaults.controller';
 import { VaultsService } from './vaults.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Vault]), ActivityLogsModule],
+  imports: [TypeOrmModule.forFeature([Vault]), ActivityLogsModule, WebhooksModule],
   controllers: [VaultsController],
   providers: [VaultsService, Logger],
   exports: [VaultsService],
