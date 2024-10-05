eea8978d9bb252d8296cb5d0b36fb2faf89b224a
sen-sourabh <sourabhsen201313@gmail.com>
2024-10-05 12:00:48 +0530

    git lint workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,49 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+      project: 'tsconfig.json',
+      tsconfigRootDir: __dirname,
+      sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
   ],
   root: true,
   env: {
-    node: true,
-    jest: true,
+      node: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+              argsIgnorePattern: '^_',
+              varsIgnorePattern: '^_',
+              caughtErrorsIgnorePattern: '^_',
+          },
+      ],
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error',
+      '@typescript-eslint/no-restricted-imports': [
+          'error',
+          {
+              patterns: ['*/src/*', 'src/*', '!../src/*', '**/index'],
+          },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': [
+          'error',
+          { ignoreTypeValueShadow: true },
+      ],
   },
 };
