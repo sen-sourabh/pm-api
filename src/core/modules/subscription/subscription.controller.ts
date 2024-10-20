9af2bc83fcdb987de11237697fd9926c777620a2
sen-sourabh <sourabhsen201313@gmail.com>
2024-10-20 12:37:05 +0530

    readme github actions updates

--- src/core/modules/subscription/subscription.controller.ts
+++ src/core/modules/subscription/subscription.controller.ts
@@ -1,37 +1,9 @@
-import { Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../shared/guards/jwt-auth.guard';
-import { SubscriptionService } from './subscription.service';
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 @ApiTags('Subscriptions')
 @Controller('subscription')
-export class SubscriptionController {
-  constructor(private readonly subscriptionService: SubscriptionService) {}
-
-  @Post()
-  create() {
-    // return this.subscriptionService.create(createSubscriptionDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.subscriptionService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.subscriptionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update() {
-    // return this.subscriptionService.update(+id, updateSubscriptionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.subscriptionService.remove(+id);
-  }
-}
+export class SubscriptionController {}
